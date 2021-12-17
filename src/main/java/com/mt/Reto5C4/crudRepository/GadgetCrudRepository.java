4132f59a4f689d288d97ef2bd4aa8dc86dd6efd4
patricia bernal <patriciabernalarias1@gmail.com>
2021-12-16 21:52:45 -0500

    Corrección de la búsqueda por precio

--- src/main/java/com/mt/Reto5C4/crudRepository/GadgetCrudRepository.java
+++ src/main/java/com/mt/Reto5C4/crudRepository/GadgetCrudRepository.java
@@ -3,12 +3,15 @@ package com.mt.Reto5C4.crudRepository;
 import com.mt.Reto5C4.model.Gadget;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 /**
  * @author  Olga Patricia Bernal
  * @since   2021-12-14
  */
+
 public interface GadgetCrudRepository extends MongoRepository<Gadget, Integer> {
+    @Query("{price:{$lte:?0}}") 
     public List <Gadget> findByPrice (double price);
     
     
